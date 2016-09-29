66b236ca287c13c1b849c8a729bf6036b32dbe24
CarlosEstrada <kachiiz@gmail.com>
2016-09-28 21:35:34 -0500

    first commit

---
+++ app/src/main/java/com/example/kachyz/contactos/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.kachyz.contactos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Contacto> contactos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactos = new ArrayList<Contacto>();
+        contactos.add(new Contacto("Carlos","123654","Carlos@gmail.com"));
+        contactos.add(new Contacto("Eduardo","132423654","Carrlos@gmail.com"));
+        contactos.add(new Contacto("Estrada","123888","Carlodds@gmail.com"));
+        contactos.add(new Contacto("Guardado","1654123654","Caaaarlos@gmail.com"));
+        contactos.add(new Contacto("Juan","12363676354","Carlo96s@gmail.com"));
+
+        ArrayList<String> nombresContacto = new ArrayList<>();
+        for (Contacto contacto:contactos) {
+            nombresContacto.add(contacto.getNombre());
+        }
+
+        ListView lstContactos = (ListView) findViewById(R.id.lstContactos);
+        lstContactos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombresContacto));
+
+        lstContactos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetalleContacto.class);
+                intent.putExtra(getResources().getString(R.string.pnombre),contactos.get(position).getNombre());
+                intent.putExtra(getResources().getString(R.string.ptelefono),contactos.get(position).getTelefono());
+                intent.putExtra(getResources().getString(R.string.pemail),contactos.get(position).getEmail());
+                startActivity(intent);
+            }
+        });
+    }
+}
+
