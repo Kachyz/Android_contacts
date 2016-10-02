93f927ce43b9e42943121b45eb1cb67309d193df
CarlosEstrada <kachiiz@gmail.com>
2016-10-01 20:11:59 -0500

    permisos y matando actividades para evitar que se acumulen

--- app/src/main/java/com/example/kachyz/contactos/DetalleContacto.java
+++ app/src/main/java/com/example/kachyz/contactos/DetalleContacto.java
@@ -7,14 +7,25 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class DetalleContacto extends AppCompatActivity {
 
     private TextView tvNombre;
     private TextView tvTelefono;
     private TextView tvEmail;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +44,27 @@ public class DetalleContacto extends AppCompatActivity {
         tvNombre.setText(nombre);
         tvTelefono.setText(telefono);
         tvEmail.setText(email);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void llamar(View v) {
         String telefono = tvTelefono.getText().toString();
-        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + telefono)));
+        if (ActivityCompat.checkSelfPermission(DetalleContacto.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telefono)));
     }
 
-    public void enviarMail(View v){
+    public void enviarMail(View v) {
         String email = tvEmail.getText().toString();
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
         emailIntent.setData(Uri.parse("mailto:"));
@@ -48,4 +72,14 @@ public class DetalleContacto extends AppCompatActivity {
         emailIntent.setType("message/rfc822");
         startActivity(Intent.createChooser(emailIntent, "Email"));
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent intent = new Intent(DetalleContacto.this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    
 }
